56d96eaed203303a668dc24c1f4b16469f3f3051
Gyeongbo Kang <gbkang@Gyeongboui-MacBookPro.local>
2026-04-30 23:20:31 +0900

    first

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+import Intersect from './Intersect'
+import GuestSnap from './GuestSnap'
+
+function App() {
+
+	const debug = process.env.NODE_ENV === 'development'?'solid':'none';
+
+	return (
+		<div id='guestsnap'>
+			
+			<div className='main-frame'>
+
+					<Intersect className='fade-in'>
+						<GuestSnap/>
+					</Intersect>
+			</div>
+		</div>
+ )
+}
+
+export default App
