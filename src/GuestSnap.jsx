90df40c5c637d8475f9d495b505557d71b0d7059
Gyeongbo Kang <gbkang@Gyeongboui-MacBookPro.local>
2026-05-01 23:21:13 +0900

    260501 23:21

--- src/GuestSnap.jsx
+++ src/GuestSnap.jsx
@@ -4,7 +4,7 @@ import Space from './Space'
 
 const sectionHeight = 6
 const sectionDivide = 3
-const photoUrl = `${import.meta.env.BASE_URL}image/weddinglocation.png`
+const photoUrl = `${import.meta.env.BASE_URL}image/guestsnapphoto.jpg`
 
 const GuestSnap = () => {
 
@@ -15,16 +15,15 @@ const GuestSnap = () => {
 	}
 
 	return (
-		
 		<div className='content-box'>
 
-			<div className='image-container'>
-				<img src={photoUrl}/>
-			</div>
-
-			<Space height={`${sectionDivide}rem`}/>
 			
-			<div className='section-subtitle'> GUEST SNAP </div>
+				<div className='image-container'>
+					<img src={photoUrl}/>
+				</div>
+
+			<Space height={`${sectionHeight}rem`}/>
+			<div className='section-subtitle'> GUEST SNAP</div>
 			<div className='section-title'> 게스트스냅 </div>
 
 			<Space height={`${sectionDivide}rem`}/>
@@ -34,33 +33,32 @@ const GuestSnap = () => {
 				width: '95%',
 				margin: '0 auto',
 			}}>
-
 				저희의 사진 작가가 되어주세요 <br/><br/>
-				신랑 신부의 행복한 순간을 담아주세요<br/>
+				신랑 신부의 행복한 순간을 담아주세요 <br/>
+				
 				예식 당일 아래의 업로드 버튼을 통해 올려주세요<br/>
+
 
 			</div>
 
 			<Space height={`${sectionDivide}rem`}/>
 
-			<div className='button' onClick={openGuestSnap}
+			<div className='button'  onClick={openGuestSnap}
 				style={{width: '15rem', margin: 'auto'}}>
-					사진 업로드
+				사진 업로드
 			</div>
 
 			<Space height={'0.5rem'}/>
-
-			<div style={{color:'gray', fontSize:'1.2rem'}}>
+			<div style={{color: 'gray', fontSize:'1.2rem'}}>
 				업로드 기한: 26/5/11 23:59까지
 			</div>
 
+		
 			<Space height={`${sectionHeight}rem`}/>
 
-		</div>
-
+		</div>	
 	)
 }
 
 export default GuestSnap
-
 
